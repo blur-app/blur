d71a3e89e2a0b4e6505678ffdd64c93a5eda5f13
Skyler Jane Clark <skylerjaneclark@gmail.com>
2019-02-09 21:14:52 -0500

    minor tweaks

--- src/graphQL/resolvers.ts
+++ src/graphQL/resolvers.ts
@@ -1,26 +1,5 @@
 import db from '../database';
 
-const userData = [
-    {
-        user_id: 1,
-        username: 'jsbond',
-        first_name: 'john',
-        last_name: 'stein'
-    },
-    {
-        user_id: 2,
-        username: 'yeetmaster',
-        first_name: 'ben',
-        last_name: 'stills'
-    },
-    {
-        user_id: 3,
-        username: 'joons',
-        first_name: 'huh',
-        last_name: 'asdfsa'
-    }
-];
-
 const getUsers: Function = () => {
     return db.getAllUsers();
 };
@@ -31,8 +10,7 @@ const getUser: Function = (args : any) => {
 
 const createUser:Function = (args:any) => {
     return db.createUser(args);
-}
-
+};
 
 // Root resolver
 export default {
